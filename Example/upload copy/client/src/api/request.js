652adfed7af8e504aa32f4029ef05662f257f225
slnn2080 <love.nn.linlin@gmail.com>
2022-01-10 23:15:21 +0900

    upload

--- Example/upload copy/client/src/api/request.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-export function request(config) {
-  const richEditorInstance = axios.create({
-    baseURL: 'http://localhost:8000'
-  })
-
-  return richEditorInstance(config)
-}
