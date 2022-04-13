782dcad19c6bebec1e5337e54c4328628081ba10
slnn2080 <love.nn.linlin@gmail.com>
2022-04-13 12:38:06 +0900

    Remove: 文件夹整理

--- KintoBackup/lexus/plugins/itemCard.js
+++
@@ -1,3 +0,0 @@
-import Vue from "vue"
-import ItemCardDefault from "../src/javascripts/contents/lineup/components/ItemCardDefault.vue";
-Vue.component("ItemCard", ItemCardDefault);
--- KintoBackup/lexus/static/customer/assets/js/footer-link.js
+++
@@ -1,3 +0,0 @@
-$('.appSpMenu__navi__subNav .appSpMenu__navi__sub a').on('click', function (e) {
-  e.stopPropagation();
-});
